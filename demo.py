694599b3643a53b3984c7d4f7c9f43a421b0f63b
PilliSiddharth <pillisiddharth@gmail.com>
2020-05-27 07:28:04 +0530

    Projectr Completed

--- demo.py
+++ demo.py
@@ -28,4 +28,31 @@ def start():
         transaction = int(input("Choose which robux transaction fits your day: "))
         if transaction == 1:
             print('Your Balance is R$',balance,'\n')
-            restart =
+            restart = input("Would you like to go back? ")
+            if restart in('NO','no'):
+                print('Thanks for using robux ATM..BYe!')
+
+        elif transaction == 2:
+            withdrawl = float(input("How much would you like to withdraw?\nR$80\nR$400\nR$80\nR$1000\nR$10000\nR$20000"))
+            if withdrawl in [80,400,1000,10000,20000]:
+                balance = balance - withdrawl
+                print("Your balance is: ", balance)
+                restart = input("Would you like to go back? ")
+                if restart in ('NO', 'no'):
+                    print('Thanks for using robux ATM..BYe!')
+            elif withdrawl != [80,400,1000,10000,20000]:
+                print("Invalid Amount, try again.")
+                restart = ('yes')
+
+
+        elif transaction == 3:
+            deposit = float(input("How much robux do you want to deposit?"))
+            balance = deposit + balance
+            print("Your balance is R$: ",balance,'\n')
+            restart = input("Would you like to go back? ")
+            if restart in ('NO', 'no'):
+                print('Thanks for using robux ATM..BYe!')
+
+        elif transaction == 4:
+            print("Thanks for using ROBUX ATM, hope you have a great day for you ahead.")
+            print("Exiting prgoram....")
